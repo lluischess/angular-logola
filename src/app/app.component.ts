33ce3853a6c948ca48361a70420ae28ce1b3d2ee
lluischess <lluischess@gmail.com>
2024-10-13 23:51:57 +0200

    footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,26 @@ import { NavComponent } from './nav/nav.component';
 import { HeaderComponent } from './header/header.component';
 import { BannerComponent } from './banner/banner.component';
 import { NovedadesComponent } from './novedades/novedades.component';
+import { PromoComponent } from './promo/promo.component';
+import { VentajasComponent } from './ventajas/ventajas.component';
+import { FooterComponent } from './footer/footer.component';
+import { EndComponent } from './end/end.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CartComponent, NavComponent, HeaderComponent,BannerComponent,NovedadesComponent],
+  imports: [
+    RouterOutlet,
+    CartComponent,
+    NavComponent,
+    HeaderComponent,
+    BannerComponent,
+    NovedadesComponent,
+    PromoComponent,
+    VentajasComponent,
+    FooterComponent,
+    EndComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
