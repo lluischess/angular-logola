d94e50f853b5a966dc77cdd01199bb0df8289dae
lluischess <lluischess@gmail.com>
2024-10-20 20:03:10 +0200

    enviar producto al product page

--- src/app/pages/page-product/page-product.component.ts
+++ src/app/pages/page-product/page-product.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { CartServiceService } from '../../shared/services/cart-service.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-page-product',
@@ -11,18 +12,20 @@ import { CartServiceService } from '../../shared/services/cart-service.service';
 })
 export class PageProductComponent {
 
-  constructor(private cartService: CartServiceService) {}  // Inyectamos el servicio
-
-  // Función para añadir productos al carrito
-  addToCart(product: any) {
-    this.cartService.addToCart(product);
-    console.log(`${product.nombre} añadido al carrito`);
-  }
-
+  public producto: any;
   productos = [
+    {
+      id: 1,
+      nombre: 'BOMBÓN NAVIDAD EN BOLSA UNITARIA PERSONALIZADA',
+      categoria: 'chocolates',
+      referencia: 'BE-015-156',
+      imagen: 'assets/images/BE-015-156.jpg',
+      medidas: 'Medidas 72 x 59 mm'
+    },
     {
       id: 2,
       nombre: 'Bote personalizado con Bolas de Cereales con Chocolate',
+      categoria: 'chocolates',
       referencia: 'EN-148',
       imagen: 'assets/images/EN-148.jpg',
       medidas: 'Medidas 72 x 59 mm'
@@ -30,6 +33,7 @@ export class PageProductComponent {
     {
       id: 3,
       nombre: 'Bote personalizado con Catanias',
+      categoria: 'chocolates',
       referencia: 'EN-134',
       imagen: 'assets/images/EN-134.jpg',
       medidas: 'Medidas 72 x 59 mm'
@@ -37,6 +41,7 @@ export class PageProductComponent {
     {
       id: 4,
       nombre: 'Catánias 35g en estuche Medium Faja',
+      categoria: 'chocolates',
       referencia: 'SS-201-003',
       imagen: 'assets/images/SS-201-003.jpg',
       medidas: 'Medidas 60 x 60 x 55 mm'
@@ -44,6 +49,7 @@ export class PageProductComponent {
     {
       id: 5,
       nombre: 'Bombon de Yogur y Fresa cubo medium',
+      categoria: 'chocolates',
       referencia: 'SS-202',
       imagen: 'assets/images/SS-202.jpg',
       medidas: 'Medidas  60 x 40 x 55 mm'
@@ -51,23 +57,96 @@ export class PageProductComponent {
     {
       id: 6,
       nombre: 'Chocolates Levels Piramid',
+      categoria: 'chocolates',
       referencia: 'CO-000',
       imagen: 'assets/images/CO-000.jpg',
       medidas: 'Medidas 13 x 13 x 9,5 cm'
     },
     {
-      id: 5,
-      nombre: 'Bombon de Yogur y Fresa cubo medium',
-      referencia: 'SS-202',
-      imagen: 'assets/images/SS-202.jpg',
-      medidas: 'Medidas  60 x 40 x 55 mm'
+      id: 7,
+      nombre: 'Cub Gajos Jelly',
+      categoria: 'caramelos',
+      referencia: 'FC-056-01',
+      imagen: 'assets/images/FC-056-01.jpg',
+      medidas: 'Medidas 5 x 5 x 5 cm'
     },
     {
-      id: 6,
-      nombre: 'Chocolates Levels Piramid',
-      referencia: 'CO-000',
-      imagen: 'assets/images/CO-000.jpg',
-      medidas: 'Medidas 13 x 13 x 9,5 cm'
+      id: 8,
+      nombre: 'Caja Pastis con Moras de Colores',
+      categoria: 'caramelos',
+      referencia: 'FC-307-01',
+      imagen: 'assets/images/FC-307-01.jpg',
+      medidas: 'Medidas 5 x 5 x 5 cm'
+    },
+    {
+      id: 9,
+      nombre: 'Tetris de Gominola',
+      categoria: 'caramelos',
+      referencia: 'FC-036',
+      imagen: 'assets/images/FC-036.jpg',
+      medidas: 'Medidas 7 x 7 x 7 cm'
+    },
+    {
+      id: 10,
+      nombre: 'Cata 9 Bombones Artesanos',
+      categoria: 'novedades',
+      referencia: 'SS-012',
+      imagen: 'assets/images/SS-012.jpg',
+      medidas: 'Medidas 10 x 10 x 2,5 cm'
+    },
+    {
+      id: 11,
+      nombre: 'CALENDARIO ADVIENTO LUX',
+      categoria: 'navidad',
+      referencia: 'NA-040',
+      imagen: 'assets/images/NA-040.jpg',
+      medidas: 'Medidas  330 x 130 x 45 mm'
+    },
+    {
+      id: 12,
+      nombre: 'CALENDARIO ADVIENTO BOMBONES LINDOR LINDT',
+      categoria: 'navidad',
+      referencia: 'NA-032-000',
+      imagen: 'assets/images/NA-032-000.jpg',
+      medidas: 'Medidas 250 x 250 x 30mm'
+    },
+    {
+      id: 12,
+      nombre: 'Estuche con Benjamín y Bombones Lindor',
+      categoria: 'navidad',
+      referencia: 'LOTE 003-01',
+      imagen: 'assets/images/LOTE-003-01.jpg',
+      medidas: 'Medidas 120 x 63 x 200 mm'
+    },
+    {
+      id: 12,
+      nombre: 'Galleta Artesana',
+      categoria: 'galletas',
+      referencia: 'FL-023',
+      imagen: 'assets/images/FL-023.jpg',
+      medidas: 'Medidas  5,4 x 4,5 cm'
     }
   ];
+
+  constructor(private cartService: CartServiceService, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    // Obtiene el ID del producto desde la ruta
+    const productId = this.route.snapshot.paramMap.get('id');
+
+    // Filtra para obtener el producto correspondiente
+    this.producto = this.productos.find(p => p.id === +productId!);
+
+    // Si no se encuentra el producto, puedes manejar el error de alguna manera
+    if (!this.producto) {
+      console.error('Producto no encontrado');
+      // Podrías redirigir a una página 404 o mostrar un mensaje de error
+    }
+  }
+
+  // Función para añadir productos al carrito
+  addToCart(product: any) {
+    this.cartService.addToCart(product);
+    console.log(`${product.nombre} añadido al carrito`);
+  }
 }
