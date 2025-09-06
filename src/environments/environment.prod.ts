343e6cc053f44bba41a8613e21e3155900570ead
lluischess <lluischess@gmail.com>
2025-09-06 16:38:42 +0200

    corregir build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://logolate-backend.onrender.com' // Cambia esto por la URL real que te dé Render
+  apiUrl: 'https://logolaback.onrender.com'
 };
